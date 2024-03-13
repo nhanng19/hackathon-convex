2c100c5a529636994a2d66667cf1569845c81881
nhanng19 <nhanng19@gmail.com>
2024-03-13 03:33:16 -0700

    added container for profile

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,5 +1,11 @@
 const Profile = () => {
-  return <>Profile</>;
+  return (
+    <section className="main-container">
+      <div className="w-full h-full max-w-4xl">
+        <main>Profile</main>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
