ff61b5d5b1571ffb67c1c59897b20d1969488d61
nhanng19 <nhanng19@gmail.com>
2024-03-22 15:15:25 -0700

    fix card width

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -46,7 +46,7 @@ const Card = ({ data, active, removeCard, handleAddRestaurant }: CardProps) => {
           transition={{ type: "tween", duration: 0.3, ease: "easeIn" }}
           whileDrag={{ cursor: "grabbing" }}
           exit={{ x: exitX }}
-          className="cursor-grab absolute flex  max-h-[80%] flex-col rounded-xl bg-gray-900 bg-clip-border text-light-1 shadow-2xl self-center items-center justify-center"
+          className="cursor-grab absolute flex  w-full max-w-[26rem] flex-col rounded-xl bg-gray-900 bg-clip-border text-light-1 shadow-2xl self-center items-center justify-center"
         >
           <div className="relative overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40 w-full h-[268px]">
             <img
